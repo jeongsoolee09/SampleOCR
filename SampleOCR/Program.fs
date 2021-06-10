4ea75828faac0746e6291e1c1564c1c6b107aacd
JeongSoo Lee <volcano0909@gmail.com>
2021-06-10 19:54:57 +0900

    add JPN OCR

--- SampleOCR/Program.fs
+++ SampleOCR/Program.fs
@@ -1,4 +1,7 @@
 ﻿[<EntryPoint>]
 let main argv: int =
+    printfn "============ Test Drive: OCR'ing from 1 to 9 ============"
     ReadSingleNumber.readFrom1To9 ()
+    printfn "============ Now, Read all JPN chars from screenshot ============"
+    ReadUma.readAllPortionsFromScreenShot ()
     0
--- SampleOCR/readUma.fs
+++ SampleOCR/readUma.fs
@@ -1,3 +1,21 @@
 module ReadUma
 
+(* Here, unlike in ReadSingleNumber,
+    we would like to read consecutive characters off a single image. *)
 
+
+let readSingleLineJPN (path: string) : unit =
+    let engine = new Tesseract.TesseractEngine ("tessdata-4.1.0", "jpn") in
+    path
+    |> Tesseract.Pix.LoadFromFile
+    |> (fun img -> engine.Process (img, Tesseract.PageSegMode.SingleLine))
+    |> (fun result -> result.GetText ())
+    |> printfn "OCR result: %s"
+
+
+let readAllPortionsFromScreenShot () =
+    List.iter readSingleLineJPN [ "resources/img/dialogue-1.png" // first dialogue option
+                                 ; "resources/img/dialogue-2.png" // second dialogue option
+                                 ; "resources/img/up-title-1.png" // Umamusume info title bar 1
+                                 ; "resources/img/up-title-2.png" // Umamusume info title bar 2
+                                ]
